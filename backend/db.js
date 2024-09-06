2f307cc95670e5823527911d51c9923d3bcb4c19
Vinithyadav08 <vinupvg2003@gmail.com>
2024-09-06 22:42:06 +0530

    commit

--- backend/db.js
+++ backend/db.js
@@ -1,29 +1,19 @@
 const express = require("express");
-
 require("dotenv").config(); // Ensure this is at the top of your file
 const mongoose = require("mongoose");
 
 const mongoDB = async () => {
   try {
     // Verify that MONGO_URI is defined
-    if (
-      "mongodb+srv://hungryhut:vinu1234@cluster0.ccgqu.mongodb.net/hungryhutmern?retryWrites=true&w=majority&appName=Cluster0"
-    ) {
+    if (!process.env.MONGO_URI) {
       throw new Error("MONGO_URI is not defined in the .env file");
     }
 
     // Connect to MongoDB
-    // await mongoose.connect(process.env.MONGO_URI, {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // });
-    await mongoose.connect(
-      "mongodb+srv://hungryhut:vinu1234@cluster0.ccgqu.mongodb.net/hungryhutmern?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected successfully to MongoDB");
 
     // Fetch data from collections
