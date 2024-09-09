662599736fef5d7d9adba401b9588b7c7b6b0bfc
Vinithyadav08 <vinupvg2003@gmail.com>
2024-09-09 23:43:49 +0530

    comm

--- backend/index.js
+++ backend/index.js
@@ -71,6 +71,10 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 app.use(express.static(path.join(__dirname, "src")));
 app.use("/api/stripe", require("./Routes/stripe"));
 app.use("/api/createuser", require("./Routes/CreateUser"));
