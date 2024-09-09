988931f4126b2e7743d71d433ec93ebac9b23105
Vinithyadav08 <vinupvg2003@gmail.com>
2024-09-09 22:45:49 +0530

    comm

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,25 @@ mongoDB();
 
 const cors = require("cors");
 
-const cors = require("cors");
-app.use(
-  cors({
-    origin: "https://hungryhutapp-mern.onrender.com", // Allow your frontend
-  })
-);
+const allowedOrigins = [
+  "https://hungryhutapp-mern.onrender.com",
+  "http://localhost:3000",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+
 
 
 
